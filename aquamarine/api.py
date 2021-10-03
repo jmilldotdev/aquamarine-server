8096b85ff8ede6fdf59c774ba090cfbee4046be6
Jonathan Miller <jon@jmill.dev>
2021-10-03 16:46:59 -0400

    connect frontend to backend

--- aquamarine/api.py
+++ aquamarine/api.py
@@ -1,11 +1,27 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from aquamarine.client import AquamarineClient
 
 app = FastAPI()
 aquamarine = AquamarineClient()
 
+origins = ["http://localhost:3000", "localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root() -> dict[str, str]:
     return {"Hello": "World"}
+
+
+def run():
+    uvicorn.run("aquamarine.api:app", host="0.0.0.0", port=8000, reload=True)
